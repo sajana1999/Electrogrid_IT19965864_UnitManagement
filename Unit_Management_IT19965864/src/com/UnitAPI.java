82e39754d158cbdbc1fdf410a8039794ed919586
IT19965864 <sajanaran@gmail.com>
2022-05-15 10:30:16 +0530

    Add services

--- Unit_Management_IT19965864/src/com/UnitAPI.java
+++ Unit_Management_IT19965864/src/com/UnitAPI.java
@@ -1,6 +1,9 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UnitAPI")
 public class UnitAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	UnitInformation unitobj = new UnitInformation();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,7 +40,64 @@ public class UnitAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String output = unitobj.insertUnits(
+				   request.getParameter("date"), 
+				   request.getParameter("validMonth"), 
+				   request.getParameter("unitRange"), 
+				   request.getParameter("unitPrice")
+				  ); 
+		response.getWriter().write(output);
 		doGet(request, response);
 	}
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+												Map paras = getParasMap(request); 
+												String output = unitobj.updateUnits(paras.get("hideUnitInformationIDUpdate").toString(),
+				 
+				 							paras.get("date").toString(), 
+				 							paras.get("validMonth").toString(), 
+				 							paras.get("unitRange").toString(), 
+				 							paras.get("unitPrice").toString() 
+				 							); 
+		response.getWriter().write(output); 
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		 String output = unitobj.deleteUnits(paras.get("unitsId").toString()); 
+		response.getWriter().write(output);
+	}
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+	 Map<String, String> map = new HashMap<String, String>(); 
+	try
+	 { 
+	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+	 String queryString = scanner.hasNext() ? 
+	 scanner.useDelimiter("\\A").next() : ""; 
+	 scanner.close(); 
+	 String[] params = queryString.split("&"); 
+	 for (String param : params) 
+	 {
+		 String[] p = param.split("=");
+		 map.put(p[0], p[1]); 
+		 } 
+		 } 
+		catch (Exception e) 
+		 { 
+		 } 
+		return map; 
+		}
 
 }
